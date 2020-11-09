d398624b51bfb2656331b4a525700f2e0967a2f5
amin havasi <pau.ahq@gmail.com>
2020-11-09 20:56:49 +0330

    pause

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,20 +1,18 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
 
-const {createProjectValidator}=require('./../validator/projectsValidator')
+const { createProjectValidator } = require('./../validator/projectsValidator');
 
 //admin projects
 
-
 ///
-router.post('/create',async(req,res)=>{
+router.post('/create', async (req, res) => {
     try {
-        const {error}=await createProjectValidator(req.body)
-    if(error)return res.status(400).send(error.details[0].message)        
+        const { error } = await createProjectValidator(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
     } catch (err) {
-        res.status(400).send(err)
-        
+        res.status(400).send(err);
     }
-})
+});
 
-module.exports=router
+module.exports = router;
